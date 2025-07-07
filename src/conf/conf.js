47558cea070988809d6e9322161a973680e48250
mahmedsaleem1 <mahmedsaleem786@gmail.com>
2025-07-08 01:35:52 +0500

    Just did project Initialization and setup of .env

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    APPWRITE_API_ENDPOINT: String(import.meta.VITE_APPWRITE_API_ENDPOINT),
+    APPWRITE_PROJECT_ID: String(import.meta.VITE_APPWRITE_PROJECT_ID),
+    APPWRITE_DATABASE_ID: String(import.meta.VITE_APPWRITE_DATABASE_ID),
+    APPWRITE_COLLECTION_ID: String(import.meta.VITE_APPWRITE_COLLECTION_ID),
+    APPWRITE_BUCKET_ID: String(import.meta.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
